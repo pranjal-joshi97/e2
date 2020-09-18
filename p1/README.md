e3824aad14208c010eef70965037a8ef228f167d
Pranjal Joshi <pjoshi1@babson.edu>
2020-09-18 16:23:38 +0700

    creation of README.md and index.php for p1

---
+++ p1/README.md
@@ -0,0 +1,1 @@
+README.md for Project 1
---
+++ p1/index.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang='en'>
+<head><title>Practice Application</title>
+	<meta charset='utf-8'>
+</head> 
+<body>
+     <h1>Project 1</h1>
+</body>
+</html>
