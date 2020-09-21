c5789799d1c00f6bf9a77792ce0fcb51bff41c53
Pranjal Joshi <pjoshi1@babson.edu>
2020-09-21 15:27:08 +0700

    added content to index-view

--- p1/index-view.php
+++ p1/index-view.php
@@ -1,9 +1,21 @@
 <!doctype html>
 <html lang='en'>
-<head><title>Project 1</title>
+<head><title>2 Dice Roller</title>
 	<meta charset='utf-8'>
 </head> 
 <body>
-     <h1>Project 1 draft</h1>
+     <h1>2 Dice Roller</h1>
+     <h3>Mechanics:</h3>
+     <ul>
+        <li>Dice 1 and Dice 2 are randomly thrown.</li> 
+        <li>Whichever dice lands on the higher value, wins.</li> 
+        <li>A tie is declared if both dice land on the same value.</li>
+     </ul>
+
+     <h3>Results:</h3>
+     <ul>
+        <li>Dice 1 rolled </li>
+        <li>Dice 2 rolled </li>
+        <li>x is the winner! </li>
 </body>
 </html>
