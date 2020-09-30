90300ba0661b4c8e10e366aef7a2950454cab4a3
Pranjal Joshi <pjoshi1@babson.edu>
2020-09-30 22:34:36 +0700

    loops.php

---
+++ practice/loops.php
@@ -0,0 +1,16 @@
+<?php
+
+// $numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// foreach ($numbers as $key => $number) {
+//     var_dump($number);
+// }
+
+for ($i = 0; $i <= 10; $i++) {
+    var_dump($i);
+}
+
+$i = 0;
+while ($i <= 10) {
+    var_dump($i);
+    $i++;
+}
