addc620544edbb91dbced102163a35ba34da92cd
Pranjal Joshi <pjoshi1@babson.edu>
2020-11-21 14:55:19 +0700

    zipfoods index page

--- zipfoods/views/templates/master.blade.php
+++ zipfoods/views/templates/master.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang='en'>
+
 <head>
 
     <title>@yield('title', $app->config('app.name'))</title>
@@ -8,23 +9,27 @@
 
     <link rel='shortcut icon' href='/favicon.ico'>
 
-    <link href='/css/app.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link href='/css/zipfoods.css' rel='stylesheet'>
 
     @yield('head')
 
 </head>
+
 <body>
 
-<header>
-    <img id='logo' src='/images/hes-logo.png' alt='Harvard Extension School Logo'>
-    <h1>{{ $app->config('app.name') }}</h1>
-</header>
+    <header>
+        <a href='/'><img id='logo' src='/images/zipfoods-logo.png' alt='{{ $app->config(' app.name') }} Logo'></a>
+        <!-- <h1>{{ $app->config('app.name') }}</h1> -->
+    </header>
 
-<main>
-    @yield('content')
-</main>
+    <main>
+        @yield('content')
+    </main>
 
-@yield('body')
+    @yield('body')
 
 </body>
+
 </html>
