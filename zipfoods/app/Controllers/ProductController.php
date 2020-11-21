29ac75f1e630b516701f8f687a22b3c28affc867
Pranjal Joshi <pjoshi1@babson.edu>
2020-11-21 16:01:22 +0700

    show products

--- zipfoods/app/Controllers/ProductController.php
+++ zipfoods/app/Controllers/ProductController.php
@@ -6,12 +6,19 @@ use App\Products;
 
 class ProductController extends Controller
 {
+    private $products;
+
+    public function __construct($app)
+    {
+        parent::__constructs($app);
+        $this->products = new Products($this->app->path('database/products.json'));
+    }
     public function index()
     {
         $products = new Products($this->app->path('database/products.json'));
 
         return $this->app->view('products.index', [
-            'products' => $products->getAll()
+            'products' => $this->products->getAll()
         ]);
     }
 
