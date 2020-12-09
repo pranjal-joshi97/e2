99847cbfe473781eb530ed9979b8f7e0b5796ffe
Pranjal Joshi <pjoshi1@babson.edu>
2020-12-09 16:23:34 +0700

    nav buttons

--- p3/views/round.blade.php
+++ p3/views/round.blade.php
@@ -8,12 +8,11 @@ Round Details
 @section('content')
 <div id="card">
     <a href='/' class="page-nav">Homepage</a>
+    <a href='/history' class="page-nav">Game History</a>
     <h2>Round Details</h2>
     <p><b class="titles">You played:</b> <b class="results">{{ $round['move'] }}</b></p>
     <p><b class="titles">Computer played:</b> <b class="results">{{ $round['computer'] }}</b></p>
     <p><b class="titles">Result:</b> <b class="results">{{ $round['result'] }}</b></p>
     <p><b class="titles">Time:</b> <b class="results">{{ $round['time'] }}</b></p>
-
-
 </div>
 @endsection
